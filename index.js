0b0b856898f917638c8e685f143b7b812a06c3a0
imskanand <the.anand3099@gmail.com>
2023-01-08 21:27:51 +0530

    Connection to the Local postgresDB connected successfully

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import { Sequelize } from 'sequelize';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const sequelize = new Sequelize('internet_folks', 'imskanand', 'imskanand', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.get('/', (request, response) => {
   response.json({
