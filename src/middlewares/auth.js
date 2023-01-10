8bec04655422fcf7ef21ffc57b2c044a46cc3a90
imskanand <the.anand3099@gmail.com>
2023-01-11 00:42:53 +0530

    Added Middleware as auth and error Handling

---
+++ src/middlewares/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = (req, res, next) => {
+  // Get the token from the header
+  const token = req.header('Authorization');
+
+  // Return an error if the token is not present
+  if (!token) {
+    return res.status(401).send({ message: 'No token, authorization denied' });
+  }
+
+  // Verify the token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).send({ message: 'Token is not valid' });
+  }
+};
